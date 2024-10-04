cf823cc8030e1230c4a77c1fc80ba1920446ae0f
youngmaster <paulmanley150@gmail.com>
2024-10-04 14:06:21 -0500

    seconde3

--- [153]+PagingApp (3)/PagingApp/app/src/main/java/com/mastercoding/pagingapp/MovieApp.java
+++
@@ -1,9 +0,0 @@
-package com.mastercoding.pagingapp;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class MovieApp extends Application {
-}
