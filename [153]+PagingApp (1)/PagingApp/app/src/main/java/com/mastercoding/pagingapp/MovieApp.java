ad8fc6298f9842d92ff89cbf6300a2e978afac95
youngmaster <paulmanley150@gmail.com>
2024-10-04 14:36:05 -0500

    seconde4

---
+++ [153]+PagingApp (1)/PagingApp/app/src/main/java/com/mastercoding/pagingapp/MovieApp.java
@@ -0,0 +1,9 @@
+package com.mastercoding.pagingapp;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MovieApp extends Application {
+}
---
+++ [153]+PagingApp/PagingApp/app/src/main/java/com/mastercoding/pagingapp/util/Utils.java
@@ -0,0 +1,7 @@
+package com.mastercoding.pagingapp.util;
+
+public class Utils {
+    public static String API_KEY = "890a86f5656fdca2767b6be3222e3526";
+    public static String BASE_URL  = "https://api.themoviedb.org/3/";
+
+}
